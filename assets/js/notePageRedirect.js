5708b288edc37d1f92390c056a80b9d6bae003d0
xienotes <sci707@163.com>
2020-05-29 09:54:53 +0800

    minior change on page navigation of ntoes

--- assets/js/notePageRedirect.js
+++ assets/js/notePageRedirect.js
@@ -1,15 +1,18 @@
 var input = document.getElementById("pageinput");
+input.addEventListener("focus", function() {
+  input.value = "";
+});
+input.addEventListener("focusout", function() {
+  input.value = input.placeholder;
+});
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
    event.preventDefault();
-   document.getElementById("myBtn").click();
+   var page_number = input.value;
+   if (page_number == "1") {
+     window.location.href="/notes/index.html";
+   } else {
+     window.location.href="/notes/page-" + page_number + "/index.html";
+   }
   }
 });
-function pageRedirect() {
-  var page_number = document.getElementById("pageinput").value;
-  if (page_number == "1") {
-    window.location.href="/notes/index.html";
-  } else {
-    window.location.href="/notes/page-" + page_number + "/index.html";
-  }
-}
